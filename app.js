9f6ad7bd88be415f7d67ffd87b5de41a009d0186
Ari Rivera <soewarsoari@gmail.com>
2020-09-07 18:40:46 -0700

    Init Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let shelf = document.querySelector('.shelf');
+let albums = shelf.getElementsByTagName('DIV');
+
+// for (let album of albums) {
+//   album.addEventListener('mouseover', () => albumFocus(album));
+// }
+
+// for (let album of albums) {
+//   album.addEventListener('mouseout', () => albumUnfocus(album));
+// }
+
+// function albumFocus(album) {
+//   album.style.setProperty('z-index', '100');
+// }
+
+// function albumUnfocus(album) {
+//   album.style.setProperty('z-index', '1');
+// }
