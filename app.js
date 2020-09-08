11e407a5554ebb763d752e318e75362fadcbcb02
Ari Rivera <soewarsoari@gmail.com>
2020-09-07 19:49:02 -0700

    Add text focus on click option

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
+// GLOBAL VARIABLES
 let shelf = document.querySelector('.shelf');
 let albums = shelf.getElementsByTagName('DIV');
 let textblocks = document.querySelector('.textblock-wrapper');
 
-function albumFocus(album) {
+// ALBUM AND TEXT FOCUS ON HOVER
+function albumFocusText(album) {
   let selectedId = album.id;
+  //   ALBUM FOCUS
   document.querySelector('.selected-album').classList.toggle('selected-album');
   album.classList.toggle('selected-album');
-
+  // TEXT FOCUS
   document.querySelector('.selected-text').classList.remove('selected-text');
   document.querySelector(`#${selectedId}-text`).classList.add('selected-text');
 }
 
+// ADD EVENT LISTENERS
 function setupHover() {
   for (let album of albums) {
-    album.addEventListener('mouseover', () => albumFocus(album));
+    album.addEventListener('mouseover', () => albumFocusText(album));
   }
 }
 
+//   ALBUM FOCUS ON HOVER (NO TEXT)
+function albumFocus(album) {
+  let selectedId = album.id;
+  document.querySelector('.selected-album').classList.toggle('selected-album');
+  album.classList.toggle('selected-album');
+}
+
+// TEXT FOCUS
+function pickText(album) {
+  let selectedId = album.id;
+  document.querySelector('.selected-text').classList.remove('selected-text');
+  document.querySelector(`#${selectedId}-text`).classList.add('selected-text');
+}
+
+// ADD EVENT LISTENERS
+function setupClick() {
+  for (let album of albums) {
+    album.addEventListener('mouseover', () => albumFocus(album));
+  }
+  for (let album of albums) {
+    album.addEventListener('click', () => pickText(album));
+  }
+}
+
+// INITIALIZE SCRIPTS
 setupHover();
+// setupClick();
