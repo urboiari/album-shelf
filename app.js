9578e8a7492048c32928bbe16a3a933731c194ce
Ari Rivera <soewarsoari@gmail.com>
2020-09-07 20:46:39 -0700

    Implement social media icons

--- app.js
+++ app.js
@@ -2,16 +2,30 @@
 let shelf = document.querySelector('.shelf');
 let albums = shelf.getElementsByTagName('DIV');
 let textblocks = document.querySelector('.textblock-wrapper');
+let iconWrapper = document.querySelector('.listen-icons');
+let iconList = iconWrapper.getElementsByTagName('DIV');
 
 // ALBUM AND TEXT FOCUS ON HOVER
 function albumFocusText(album) {
   let selectedId = album.id;
-  //   ALBUM FOCUS
+  // ALBUM FOCUS
   document.querySelector('.selected-album').classList.toggle('selected-album');
   album.classList.toggle('selected-album');
   // TEXT FOCUS
   document.querySelector('.selected-text').classList.remove('selected-text');
   document.querySelector(`#${selectedId}-text`).classList.add('selected-text');
+}
+
+function toggleIconOpacityOff(icon) {
+  for (let item of iconList) {
+    if (item != icon) item.style.opacity = '0.5';
+  }
+}
+
+function toggleIconOpacityOn() {
+  for (let icon of iconList) {
+    icon.style.opacity = '1';
+  }
 }
 
 // ADD EVENT LISTENERS
@@ -21,6 +35,19 @@ function setupHover() {
   }
 }
 
+function setupIcons() {
+  for (let icon of iconList) {
+    icon.addEventListener('mouseover', () => toggleIconOpacityOff(icon));
+    icon.addEventListener('mouseout', () => toggleIconOpacityOn());
+  }
+}
+
+// INITIALIZE SCRIPTS
+setupHover();
+setupIcons();
+// setupClick();
+
+// UNUSED SCRIPTS
 //   ALBUM FOCUS ON HOVER (NO TEXT)
 function albumFocus(album) {
   let selectedId = album.id;
@@ -44,7 +71,3 @@ function setupClick() {
     album.addEventListener('click', () => pickText(album));
   }
 }
-
-// INITIALIZE SCRIPTS
-setupHover();
-// setupClick();
